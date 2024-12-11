329b149e181037cbc4401f635214f5ec8549f83f
Hugo Ljungqvist <hulj07001@utb.vaxjo.se>
2024-12-11 12:00:23 +0100

    Add project files.

---
+++ inlämning4_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace inlämning4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i <= 30; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for (int i = 200; i >= 180 ; i--)
+            {
+                Console.WriteLine(i);
+            }
+            for (int i = 1000 ; i <= 1400; i=i+50)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
